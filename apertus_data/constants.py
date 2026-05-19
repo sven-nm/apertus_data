626873caf9ff99d089b7bed63742838e786e800a
sven-nm <49749166+sven-nm@users.noreply.github.com>
2026-05-19 16:48:52 +0200

    First draft version -- adds `constants.py`

---
+++ apertus_data/constants.py
@@ -0,0 +1,38 @@
+"""Centralised constants and paths for apertus_data."""
+
+from pathlib import Path
+import yaml
+
+PROJECT_ROOT: Path = Path(__file__).resolve().parent.parent
+CATALOGUE_DIR: Path = PROJECT_ROOT / 'catalogue'
+BUILDERS_DIR: Path = PROJECT_ROOT / 'builders' # Todo change this
+DATA_DIR: Path = Path('/Users/sven/Desktop/data')  # Todo: ⚠️ change this
+
+SCHEMA_PATH = PROJECT_ROOT / 'apertus_data' / 'dataset_schema.yaml'
+
+GITHUB_OWNER = 'sven-nm'  # Todo: ⚠️ change this
+GITHUB_REPO = 'apertus_data'  # Todo: ⚠️ change this
+GITHUB_RAW_BASE = f'https://raw.githubusercontent.com/{GITHUB_OWNER}/{GITHUB_REPO}'
+
+YAML_KEYS = yaml.safe_load(SCHEMA_PATH.read_text(encoding='utf-8'))['properties'].keys()
+
+# Todo: 👀  will this be necessary ? Remove eventually.
+# MANUAL_YAML_KEYS = [
+#     'url',
+#     'owner',
+#     'name',
+#     'version',
+#     'tags',
+#     'modalities',
+#     'languages',
+#     'licenses',
+#     'formats',
+#     'notes'
+# ]
+#
+# AUTOMATIC_YAML_KEYS = [
+#     'id',
+#     'build_requirements',
+#     'build_history',
+#     'usage_history'
+# ]
